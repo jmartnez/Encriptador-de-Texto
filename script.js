9fa0f7f71fa1dd5bebd42c86e57c5b5947164f72
Juan Martinez <111025032+jmartnez@users.noreply.github.com>
2024-08-27 21:13:43 -0300

    Subiendo proyecto
    Subiendo proyecto de encriptador.

---
+++ script.js
@@ -0,0 +1,48 @@
+const textArea = document.querySelector(".texto-area");
+const mensaje = document.querySelector(".mensaje");
+const btnCopiar = document.getElementById("btn-copiar");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    btnCopiar.style.display = "block";
+}
+
+function encriptar (stringEncriptada) {
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["enter","e"],["imes","i"],["ai","a"],["ober","o"],["ufat","u"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][0])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringDesencriptada;
+}
+
+function copiarTexto() {
+    mensaje.select();
+    document.execCommand("copy");
+}
+
+btnCopiar.addEventListener("click", copiarTexto);
